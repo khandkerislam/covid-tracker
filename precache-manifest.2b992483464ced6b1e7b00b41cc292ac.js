7626788442d8cdee78829514521f5217fc059098
Khandker <khandker.islam46@gmail.com>
2021-05-31 12:46:09 -0400

    Updates

---
+++ precache-manifest.2b992483464ced6b1e7b00b41cc292ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52aaba32881cd29a183c5083af8c5135",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b61dc14543119493b0b2",
+    "url": "/static/css/main.9216f899.chunk.css"
+  },
+  {
+    "revision": "4f70972657c12adccbf3",
+    "url": "/static/js/2.f101d815.chunk.js"
+  },
+  {
+    "revision": "5e5c5aaefd726bfc2ad51722d14c5d57",
+    "url": "/static/js/2.f101d815.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b61dc14543119493b0b2",
+    "url": "/static/js/main.22fbce22.chunk.js"
+  },
+  {
+    "revision": "ec3bfbe4aae75bafd98e",
+    "url": "/static/js/runtime-main.4755aadd.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
